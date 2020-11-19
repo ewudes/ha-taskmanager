6ab8bc7b369b3c51f86f15da577d1b197914f3d8
ewudes <ewudes@gmail.com>
2020-11-19 16:50:56 +0400

    added the board and task modules

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createSiteMenuTemplate } from "./view/site-menu.js";
 import { createFilterTemplate } from "./view/filter.js";
+import { createTaskTemplate } from "./view/task.js";
+import { createBoardTemplate } from "./view/board.js";
+
+const TASK_COUNT = 3;
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -10,3 +14,11 @@ const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
 render(siteMainElement, createFilterTemplate(), `beforeend`);
+render(siteMainElement, createBoardTemplate(), `beforeend`);
+
+const boardElement = siteMainElement.querySelector(`.board`);
+const taskListElement = boardElement.querySelector(`.board__tasks`);
+
+for (let i = 0; i < TASK_COUNT; i++) {
+  render(taskListElement, createTaskTemplate(), `beforeend`);
+}
