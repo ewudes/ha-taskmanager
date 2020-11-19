bcb11a2e4feadb4451ec037b09ab83694837ab8e
ewudes <ewudes@gmail.com>
2020-11-19 16:59:20 +0400

    added the load-more-button module

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createSiteMenuTemplate } from "./view/site-menu.js";
 import { createFilterTemplate } from "./view/filter.js";
 import { createTaskTemplate } from "./view/task.js";
 import { createTaskEditTemplate } from "./view/task-edit.js";
+import { createLoadMoreButtonTemplate } from "./view/load-more-button.js";
 import { createBoardTemplate } from "./view/board.js";
 
 const TASK_COUNT = 3;
@@ -25,3 +26,5 @@ render(taskListElement, createTaskEditTemplate(), `beforeend`);
 for (let i = 0; i < TASK_COUNT; i++) {
   render(taskListElement, createTaskTemplate(), `beforeend`);
 }
+
+render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
