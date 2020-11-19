546765af8b414bd8652b3f5c95567f9b12c626ef
ewudes <ewudes@gmail.com>
2020-11-19 17:36:46 +0400

    bug fix

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import { createSiteMenuTemplate } from "./view/site-menu.js";
-import { createFilterTemplate } from "./view/filter.js";
-import { createTaskTemplate } from "./view/task.js";
-import { createTaskEditTemplate } from "./view/task-edit.js";
-import { createLoadMoreButtonTemplate } from "./view/load-more-button.js";
-import { createBoardTemplate } from "./view/board.js";
+import {createSiteMenuTemplate} from "./view/site-menu.js";
+import {createFilterTemplate} from "./view/filter.js";
+import {createTaskTemplate} from "./view/task.js";
+import {createTaskEditTemplate} from "./view/task-edit.js";
+import {createLoadMoreButtonTemplate} from "./view/load-more-button.js";
+import {createBoardTemplate} from "./view/board.js";
 
 const TASK_COUNT = 3;
 
