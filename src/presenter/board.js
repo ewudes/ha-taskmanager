9f01ca7afc6613e5d35e0cde8bdd8862ef827f52
ewudes <ewudes@gmail.com>
2021-01-18 23:28:39 +0400

    describing the logic of the board presenter

--- src/presenter/board.js
+++ src/presenter/board.js
@@ -7,6 +7,8 @@ import TaskEditView from "../view/task-edit.js";
 import LoadMoreButtonView from "../view/load-more-button.js";
 import {render, RenderPosition} from "../utils/render.js";
 
+const TASK_COUNT_PER_STEP = 8;
+
 export default class Board {
   constructor(boardContainer) {
     this._boardContainer = boardContainer;
@@ -19,9 +21,14 @@ export default class Board {
 
   init(boardTasks) {
     this._boardTasks = boardTasks.slice();
+
+    render(this._boardContainer, this._boardComponent, RenderPosition.BEFOREEND);
+    render(this._boardComponent, this._taskListComponent, RenderPosition.BEFOREEND);
+
+    this._renderBoard();
   }
 
-  _renderTasks() {
+  _renderTasks(from, to) {
 
   }
 
@@ -34,6 +41,17 @@ export default class Board {
   }
 
   _renderBoard() {
+    if (this._boardTasks.every((task) => task.isArchive)) {
+      this._renderNoTasks();
+      return;
+    }
 
+    this._renderSort();
+
+    this._renderTasks(0, Math.min(this._boardTasks.length, TASK_COUNT_PER_STEP));
+
+    if (this._boardTasks.length > TASK_COUNT_PER_STEP) {
+      this._renderLoadMoreButton();
+    }
   }
 }
