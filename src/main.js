55a51e7a4b25aa31f5116ebb66ff66364a75bc3e
ewudes <ewudes@gmail.com>
2021-02-25 22:01:24 +0400

    application initialization refactoring

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import SiteMenuView from "./view/site-menu.js";
 import StatisticsView from "./view/statistics.js";
-import {generateTask} from "./mock/task.js";
 import BoardPresenter from "./presenter/board.js";
 import FilterPresenter from "./presenter/filter.js";
 import TasksModel from "./model/tasks.js";
@@ -9,31 +8,19 @@ import {render, RenderPosition, remove} from "./utils/render.js";
 import {MenuItem, UpdateType, FilterType} from "./const.js";
 import Api from "./api.js";
 
-const TASK_COUNT = 22;
 const AUTHORIZATION = `Basic hS2sd3dfSwcl1sa2j`;
 const END_POINT = `https://13.ecmascript.pages.academy/task-manager`;
 
-const tasks = new Array(TASK_COUNT).fill().map(generateTask);
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+
 const api = new Api(END_POINT, AUTHORIZATION);
 
-api.getTasks().then((tasks) => {
-  console.log(tasks);
-  // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
-  // а ещё на сервере используется snake_case, а у нас camelCase.
-  // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
-  // Есть вариант получше - паттерн "Адаптер"
-});
-
 const tasksModel = new TasksModel();
-tasksModel.setTasks(tasks);
 
 const filterModel = new FilterModel();
 
-const siteMainElement = document.querySelector(`.main`);
-const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 const siteMenuComponent = new SiteMenuView();
-
-render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
 
 const boardPresenter = new BoardPresenter(siteMainElement, tasksModel, filterModel);
 const filterPresenter = new FilterPresenter(siteMainElement, filterModel, tasksModel);
@@ -69,5 +56,10 @@ const handleSiteMenuClick = (menuItem) => {
 
 siteMenuComponent.setMenuClickHandler(handleSiteMenuClick);
 
+render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
 filterPresenter.init();
 boardPresenter.init();
+
+api.getTasks().then((tasks) => {
+  tasksModel.setTasks(tasks);
+});
